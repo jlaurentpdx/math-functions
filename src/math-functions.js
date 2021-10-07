bbdd559e8587210fa53b93353fafdd332d5cb3b6
Jordan Laurent <jlaurent.pdx@gmail.com>
2021-10-07 16:11:39 -0700

    fixed problem 5

--- src/math-functions.js
+++ src/math-functions.js
@@ -108,7 +108,10 @@ you may continue to use the + operator for string concatenation.
 */
 
 export function multiplyArrayWithThreeNumbers(multArr) { //eslint-disable-line
-
+    const prodArray = multiply(multiply(multArr[0], multArr[1])[0], multArr[2])[0];
+    const str = `The numbers ${multArr[0]},${multArr[1]},${multArr[2]} have a product of ${prodArray}.`;
+    
+    return [prodArray, str];
 }
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
@@ -129,7 +132,7 @@ This function should be dynamic, accepting an array of any length.
 */
 
 export function multiplyAnyArray(dynamicArray) { //eslint-disable-line
-
+    
 }
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
